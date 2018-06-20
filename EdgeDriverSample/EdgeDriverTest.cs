5177316bf7123310d68db7b01b90c12221ada769
Clay Martin <clmartin@microsoft.com>
2018-06-20 14:52:13 -0700

    Add EdgeDriver sample

---
+++ EdgeDriverSample/EdgeDriverTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Edge;
+
+namespace EdgeDriverSample
+{
+    [TestClass]
+    public class EdgeDriverTest
+    {
+        EdgeDriver driver;
+        [TestInitialize]
+        public void EdgeDriverInitialize()
+        {
+            driver = new EdgeDriver();
+        }
+
+        [TestMethod]
+        public void VerifyPageTitle()
+        {
+            driver.Url = "https://www.bing.com";
+            Assert.AreEqual("Bing", driver.Title);
+        }
+
+        [TestCleanup]
+        public void EdgeDriverCleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ EdgeDriverSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EdgeDriverSample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EdgeDriverSample")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e4c7e7f-0ae2-4712-9560-fc088d12b87c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
